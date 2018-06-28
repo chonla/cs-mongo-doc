d4a86e1f0fdddd44bbad0a7b842551b4f0c48ea7
Chonlasith Jucksriporn <chonlasith@gmail.com>
2018-06-28 12:01:31 +0700

    Apply bootstrap

--- export/html.py
+++ export/html.py
@@ -14,22 +14,22 @@ class html(exporter):
 
     def print_timestamp(self):
         ts = datetime.now().strftime("%A, %d. %B %Y %I:%M%p")
-        return ["<div class='footer'>", f"auto-generated on {ts}", "</div>"]
+        return ["<div class='footer text-right' style='margin-top:15px;'>", f"auto-generated on {ts}", "</div>"]
 
     def print_table_title(self, title):
-        return [f"<a name='{title}'></a>", "<h2>", title, "</h2>"]
+        return ["<div class='card' style='padding:15px;margin-top:20px;'><div class='card-body'>", f"<a name='{title}'></a>", "<h4 class='card-title'>", title, "</h4><p class='card-text'>"]
 
     def print_table_header_columns(self, header_columns):
-        cols = "<table><thead><tr><th>" + \
-            "</th><th>".join(header_columns) + "</th></tr></thead><tbody>"
+        cols = "<table class='table table-striped'><thead><tr><th>" + \
+            ("</th><th>".join(header_columns)) + "</th></tr></thead><tbody>"
         return [cols]
 
     def print_link(self, text, href):
         return f"<a href='#{href}'>{text}</a>"
 
     def print_row(self, data):
-        row = "<tr><td>" + "</td><td>".join(data) + "</td><tr>"
+        row = "<tr><td>" + ("</td><td>".join(data)) + "</td></tr>"
         return row
 
     def print_end_table(self, header_columns):
-        return "</tbody></table>"
+        return "</tbody></table></p></div></div>"
